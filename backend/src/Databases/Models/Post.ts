74b508aa55a2c882a09f1499f1e823789d1c0e16
thedtvn <thedtvn@users.noreply.github.com>
2025-03-06 14:51:35 +0700

    add Payment model with new fields for paymentId, paymentCode, status, userId, and class details

--- backend/src/Databases/Models/Post.ts
+++ backend/src/Databases/Models/Post.ts
@@ -1,25 +1,11 @@
 import { model, Schema } from "mongoose";
 
 const post = new Schema({
-    paymentId: { type: String, required: true, unique: true },
-    paymentCode: [{
-        code: { type: String, required: true, unique: true },
-        used: { type: Boolean, default: false },
-    }],
-    status: { type: String, enum: ["paid", "pending", "pending+"], default: "pending" },
-    paid: { type: Number, default: 0 },
-    userId: { type: String, required: true },
-    classs: [{ 
-        classId: { type: String, required: true },
-        className: { type: String, required: true },
-        amount: { type: Number, required: true },
-    }],
-    other: [
-        {
-            name: { type: String, required: true },
-            amount: { type: Number, required: true },
-        }
-    ],
+    postId: { type: String, required: true, unique: true },
+    slug: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    files: { type: [String], required: true },
 });
 
 const postModel = model('users', post);
